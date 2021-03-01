4db4a897ca0a6705da6647d9c177c90fe6b006c9
Karandeep Singh <skarandeep335@gmail.com>
2021-03-01 00:28:55 -0800

    Added JwtRequest Filter and marking it as component. Extending OncePerRequestFilter that will execute one time for a request.

---
+++ src/main/java/com/project/config/JwtRequestFilter.java
@@ -0,0 +1,21 @@
+package com.project.config;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+// marking JwtRequestFilter class as a component and extending OncePerRequestFilter that will execute one time for a request
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+    }
+
+}
