3f44404b9f7b70ec131f32b6092ed884ffef07d9
Karandeep Singh <skarandeep335@gmail.com>
2021-03-01 09:22:48 -0800

    Added comment for AssetService.

--- src/main/java/com/project/service/AssetService.java
+++ src/main/java/com/project/service/AssetService.java
@@ -15,18 +15,22 @@ public class AssetService {
     @Autowired
     private AssetRepository assetRepository;
 
+    // Method to get list of all assets
     public List<Asset> getAllAssets() {
         return (List<Asset>) assetRepository.findAll();
     }
 
+    // Method to get one asset finding by their 'id' else throwing exception if does not exist
     public Asset getAssetById(int id) {
         return assetRepository.findById(id).orElseThrow(() -> new NotFoundException("Asset not found"));
     }
 
+    // Method to save the asset
     public void addOrUpdateAsset(Asset asset) {
         assetRepository.save(asset);
     }
 
+    // Method to delete the asset by their id
     public void deleteAsset(int id) {
         assetRepository.deleteById(id);
     }
