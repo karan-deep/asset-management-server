072625f115dcaa7d1f7018bb053d9a732be2bacd
Karandeep Singh <skarandeep335@gmail.com>
2021-03-01 00:41:16 -0800

    Overriding doFilterInternal method that will be invoked once per request within a single request thread. It will read the token, validate it and will make the user authenticated.

--- src/main/java/com/project/config/JwtRequestFilter.java
+++ src/main/java/com/project/config/JwtRequestFilter.java
@@ -8,8 +8,13 @@ import javax.servlet.http.HttpServletResponse;
 import com.project.service.UserService;
 import com.project.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import io.jsonwebtoken.ExpiredJwtException;
 
 // marking JwtRequestFilter class as a component and extending OncePerRequestFilter that will execute one time for a request
 @Component
@@ -21,10 +26,33 @@ public class JwtRequestFilter extends OncePerRequestFilter {
     @Autowired
     private JwtUtil jwtUtil;
 
+    // Method will be invoked once per request within a single request thread. This method will take the token from the authorization header and then will provide username from the token by parsing it. If token is expired, it will give an error message. Then it will validate token, after it make the current user authenticated.
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
+        String requestTokenHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwtToken = null;
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+            jwtToken = requestTokenHeader.substring(7);
+            try {
+                username = jwtUtil.getUsernameFromToken(jwtToken);
+            } catch (ExpiredJwtException e) {
+                System.out.println("JWT Token has expired");
+            }
+        }
 
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails user = userService.loadUserByUsername(username);
+            if (jwtUtil.validateToken(jwtToken, user)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        user, null, user.getAuthorities());
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        chain.doFilter(request, response);
     }
 
 }
