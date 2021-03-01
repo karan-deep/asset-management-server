1f455736e3414f9ca2f6e8ecb6047053d018825c
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 21:50:43 -0800

    Added parameterized constructor in user class.

--- src/main/java/com/project/model/User.java
+++ src/main/java/com/project/model/User.java
@@ -17,5 +17,9 @@ public class User {
     private String username;
     private String password;
 
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
 }
