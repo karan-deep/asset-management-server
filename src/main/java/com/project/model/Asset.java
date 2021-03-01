76bbbaeb8762a10bcec30a4b13314007cdf584ec
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 21:56:42 -0800

    Added asset class with their data members.

---
+++ src/main/java/com/project/model/Asset.java
@@ -0,0 +1,14 @@
+package com.project.model;
+
+import java.util.Calendar;
+
+public class Asset {
+
+    private int id;
+    private int assetTypeId;
+    private String name;
+    private String description;
+    private double price;
+    private Calendar purchaseDate;
+
+}
