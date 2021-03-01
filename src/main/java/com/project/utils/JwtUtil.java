ca7c5d34e9dabe001e979ffcb3b3888692a4c82e
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 23:47:37 -0800

    Added method to getUsername from the token.

--- src/main/java/com/project/utils/JwtUtil.java
+++ src/main/java/com/project/utils/JwtUtil.java
@@ -1,9 +1,16 @@
 package com.project.utils;
 
+import java.util.Date;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 // marking JwtUtil class as component
 @Component
 public class JwtUtil{
 
-}
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+    }
