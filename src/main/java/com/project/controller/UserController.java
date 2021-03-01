170043cd21a2a86a547317c4d8600e8313bfcc78
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 22:55:53 -0800

    Added UserController class and marking it as RestController for creating Restful controllers, it also adds the @Controller and @ResponseBody annotations.

---
+++ src/main/java/com/project/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.project.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+// marking class UserController as RestController for creating Restful controllers, it also adds the @Controller and @ResponseBody annotations
+@RestController
+public class UserController {
+
+
+}
