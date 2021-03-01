20e0ac5ac3a151e11eb408139e5c1ae787a684c2
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 22:35:19 -0800

    Added CRUD methods for assets in AssetService.

--- src/main/java/com/project/service/AssetService.java
+++ src/main/java/com/project/service/AssetService.java
@@ -1,13 +1,34 @@
 package com.project.service;
 
+import com.project.exception.NotFoundException;
+import com.project.model.Asset;
 import com.project.repository.AssetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+//marking AssetService class as an Service
 @Service
 public class AssetService {
 
     @Autowired
     private AssetRepository assetRepository;
+
+    public List<Asset> getAllAssets() {
+        return (List<Asset>) assetRepository.findAll();
+    }
+
+    public Asset getAssetById(int id) {
+        return assetRepository.findById(id).orElseThrow(() -> new NotFoundException("Asset not found"));
+    }
+
+    public void addOrUpdateAsset(Asset asset) {
+        assetRepository.save(asset);
+    }
+
+    public void deleteAsset(int id) {
+        assetRepository.deleteById(id);
+    }
 
 }
