a12d0b5d8ec0145b3ffe40b14ec324ded1b3533c
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 21:46:18 -0800

    Importing lombok to generate getter, setter and constructor in user class

--- src/main/java/com/project/model/User.java
+++ src/main/java/com/project/model/User.java
@@ -1,10 +1,18 @@
 package com.project.model;
 
-public class User {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+//Generating non-parameterized constructor, getter and setter using lombok
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
     private int id;
     private String username;
     private String password;
 
 }
 
+}
