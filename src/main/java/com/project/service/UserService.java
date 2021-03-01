c5c50610fdf40c50f03f024ad6f627cf294d1b4e
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 22:27:00 -0800

    Overriding loadUserByUsername method and returning user object if found.

--- src/main/java/com/project/service/UserService.java
+++ src/main/java/com/project/service/UserService.java
@@ -2,11 +2,14 @@ package com.project.service;
 
 import com.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-//marking class as an Service
+import java.util.ArrayList;
+
 @Service
 public class UserService implements UserDetailsService {
 
@@ -15,4 +18,13 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        com.project.model.User user = userRepository.findByUsername(username);
+        if(user!=null){
+            return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
+        }
+        return null;
+    }
 }
