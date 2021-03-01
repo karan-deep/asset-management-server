f74f6a070988f82158ee5fcf2d197075ea054c3a
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 22:58:42 -0800

    Enabling cross-origin resource sharing by using @CrossOrigin annotation for UserContoller.

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.project.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+// Enabling cross-origin resource sharing
+@CrossOrigin
 // marking class UserController as RestController for creating Restful controllers, it also adds the @Controller and @ResponseBody annotations
 @RestController
 public class UserController {
