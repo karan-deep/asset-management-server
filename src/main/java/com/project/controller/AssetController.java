e422506c2c54b93668c7972f31b8149135953ca1
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 22:39:05 -0800

    Added AssetController class and marking it as RestController for creating Restful controllers, it also adds the @Controller and @ResponseBody annotations.

---
+++ src/main/java/com/project/controller/AssetController.java
@@ -0,0 +1,9 @@
+package com.project.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+//marking class AssetController as RestController for creating Restful controllers, it also adds the @Controller and @ResponseBody annotations
+@RestController
+public class AssetController {
+
+}
