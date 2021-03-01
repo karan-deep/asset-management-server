8047b43550d89214643534da8c43009d007b9b08
Karandeep Singh <skarandeep335@gmail.com>
2021-03-01 01:00:26 -0800

    Injecting UserService and JwtRequestFilter dependency using @Autowired annotation in WebSecurityConfig.

--- src/main/java/com/project/config/WebSecurityConfig.java
+++ src/main/java/com/project/config/WebSecurityConfig.java
@@ -1,9 +1,19 @@
 package com.project.config;
 
+import com.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 // WebSecurityConfig class using @Configuration annotation indicates that a class declares one or more @Bean methods, Used @EnableWebSecurity is used for spring security configuration that finds @Configuration and automatically apply the class to the global WebSecurity.
 @Configuration
@@ -11,4 +21,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-}
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
