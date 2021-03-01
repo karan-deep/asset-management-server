75888c6dcc64731a557db05ea32a7537434d984f
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 22:20:23 -0800

    Injecting userRepository and PasswordEncoder dependencies using @Autowired annotation in UserService.

--- src/main/java/com/project/service/UserService.java
+++ src/main/java/com/project/service/UserService.java
@@ -1,10 +1,18 @@
 package com.project.service;
 
+import com.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 //marking class as an Service
 @Service
 public class UserService implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 }
