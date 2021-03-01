d8e5736f3ef1b8dc9ba079f7fd4a775952c0bef4
Karandeep Singh <skarandeep335@gmail.com>
2021-02-28 22:08:14 -0800

    Added UserRepository that is extending CrudRepository interface which provides method to do crud operation.

---
+++ src/main/java/com/project/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.project.repository;
+
+import com.project.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    User findByUsername(String username);
+}
