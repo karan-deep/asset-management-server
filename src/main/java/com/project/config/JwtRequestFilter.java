6e130a406826dbaeab53462a18c575af9d69fcea
Karandeep Singh <skarandeep335@gmail.com>
2021-03-01 00:30:26 -0800

    Injecting UserService and AssetService dependency using @Autowired annotation in JwtRequestFilter.

--- src/main/java/com/project/config/JwtRequestFilter.java
+++ src/main/java/com/project/config/JwtRequestFilter.java
@@ -5,12 +5,21 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.project.service.UserService;
+import com.project.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 // marking JwtRequestFilter class as a component and extending OncePerRequestFilter that will execute one time for a request
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
